c2144f9a02802c3c7b6f312d95e26b71539600a5
Nikita Joseph <nikita.joseph@spsmail.cuny.edu>
2014-09-17 14:56:35 -0400

    Task 1 added still need to fix

--- task_01.py
+++ task_01.py
@@ -7,11 +7,28 @@ KIM= data.SHAKESPEARE.split("\n")
 
 print KIM[0]
 MAXIMUM_WORDS=0
-COUNTER = 0
+COUNT = 0
+TEMP= 0
+TEMP2= 0
+NUM_CRISPIAN = 0
 for x in data.SHAKESPEARE.split("\n"):
-    if len(x.split()) < COUNTER:
-        MAXIMUM_WORDS = len(x.split())
-    COUNTER = COUNTER + 1
+    if len(x.split()) > TEMP:
+        MAXIMUM_WORDS= len(x.split())
+        TEMP = len(x.split())
+    COUNT = COUNT + 1
+    if "Crispian" in x:
+        NUM_CRISPIAN = NUM_CRISPIAN + 1
 
-print COUNTER
+for y in data.SHAKESPEARE.split("\n"):
+    if len(y.split()) < TEMP2:
+        MIN = len(y.split())
+    TEMP2= len(y.split())
+
+print COUNT
 print MAXIMUM_WORDS
+print MIN
+TOTAL_WORDS = len(data.SHAKESPEARE.split())
+print TOTAL_WORDS
+AVERAGE_WORDS = float(TOTAL_WORDS) / COUNT
+print AVERAGE_WORDS
+print NUM_CRISPIAN
