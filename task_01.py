833c54de70ed700ab1ef51dc7bc38509b5327c97
Nikita Joseph <nikita.joseph@spsmail.cuny.edu>
2014-09-17 15:00:49 -0400

    fixed task 01 need to work on rest

--- task_01.py
+++ task_01.py
@@ -3,17 +3,14 @@
 """Analyzing a string"""
 
 import data
-KIM= data.SHAKESPEARE.split("\n")
-
-print KIM[0]
-MAXIMUM_WORDS=0
+MAXIMUM_WORDS =0
 COUNT = 0
-TEMP= 0
-TEMP2= 0
+TEMP = 0
+TEMP2 = 0
 NUM_CRISPIAN = 0
 for x in data.SHAKESPEARE.split("\n"):
     if len(x.split()) > TEMP:
-        MAXIMUM_WORDS= len(x.split())
+        MAXIMUM_WORDS = len(x.split())
         TEMP = len(x.split())
     COUNT = COUNT + 1
     if "Crispian" in x:
@@ -21,14 +18,9 @@ for x in data.SHAKESPEARE.split("\n"):
 
 for y in data.SHAKESPEARE.split("\n"):
     if len(y.split()) < TEMP2:
-        MIN = len(y.split())
-    TEMP2= len(y.split())
+        MINIMUM_WORDS = len(y.split())
+    TEMP2 = len(y.split())
 
-print COUNT
-print MAXIMUM_WORDS
-print MIN
+print data.SHAKESPEARE
 TOTAL_WORDS = len(data.SHAKESPEARE.split())
-print TOTAL_WORDS
 AVERAGE_WORDS = float(TOTAL_WORDS) / COUNT
-print AVERAGE_WORDS
-print NUM_CRISPIAN
