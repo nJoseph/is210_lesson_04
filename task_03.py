afdeba7dba4bb27994ce7e22800eb1cbe44236c3
Nikita Joseph <nikita.joseph@spsmail.cuny.edu>
2014-09-18 11:33:07 -0400

    Fixing task 3

--- task_03.py
+++ task_03.py
@@ -2,11 +2,20 @@
 # -*- coding: utf-8 -*-
 """Looping mathematical calculations"""
 
+LEAST = 0
+COUNTER = 1
 TOTAL = 0
+MAXIMUM = 0
 import data
 for x in data.TRANSACTIONS:
     for y in x:
         TOTAL = TOTAL + y
-    if y < TOTAL:
-        MINIMUM = y
-        
+
+for z in data.TRANSACTIONS:
+    if z > data.TRANSACTIONS[COUNTER]:
+        MAX = z
+        COUNTER = COUNTER + 1
+
+for y in MAX:
+    MAXIMUM = MAXIMUM + y
+
